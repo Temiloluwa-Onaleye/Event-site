36124f2e9e9a6a0b38f5cc504423ea9a38706a05
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-09-13 01:55:05 +0100

    lifting state

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50ecce98bc3c7c4a"; }
+/******/ 		__webpack_require__.h = function() { return "5c902d84c2fc186a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -3,12 +3,24 @@ import { data } from "../../SpeakerData";
 import SpeakersList from "./SpeakersList";
 import Header from "./Header";
 import SpeakersToolbar from "./SpeakersToolbar";
+import { useState } from "react";
 
 const Speakers = () => {
+  const [showSessions, setShowSessions] = useState(true);
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="container-fluid">
-      <Header />
-      <SpeakersToolbar />
+    <div
+      className={
+        theme === "light" ? "container-fluid light" : "container-fluid dark"
+      }
+    >
+      <Header theme={theme} />
+      <SpeakersToolbar
+        theme={theme}
+        setTheme={setTheme}
+        showSessions={showSessions}
+        setShowSessions={setShowSessions}
+      />
       <SpeakersList data={data} />
     </div>
   );
