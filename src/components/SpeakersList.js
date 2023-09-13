bfd6a7febda6bfa1641c80e190c87f1f54d21058
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-09-13 21:50:56 +0100

    Error handling and loading state

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -4,6 +4,9 @@ import { data } from "../../SpeakerData";
 
 const SpeakersList = ({ showSessions }) => {
   const [speakerData, setSpeakerData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasErrored, setHasErrored] = useState(false);
+  const [error, setError] = useState("");
 
   function onFavoriteToggle(id) {
     const speakerRecPrevious = speakerData.find(function (rec) {
@@ -24,31 +27,52 @@ const SpeakersList = ({ showSessions }) => {
 
   useEffect(() => {
     async function delayFunc() {
-      await delay(2000);
-      setSpeakerData(data);
+      try {
+        await delay(2000);
+        setIsLoading(false);
+        setSpeakerData(data);
+      } catch (e) {
+        setIsLoading(false);
+        setHasErrored(true);
+        setError(e);
+      }
     }
 
     delayFunc();
   }, []);
-  return (
-    <div>
-      <div className="container speakers-list">
-        <div className="row">
-          {speakerData.map((speaker) => {
-            return (
-              <Speaker
-                key={speaker.id}
-                speaker={speaker}
-                showSessions={showSessions}
-                onFavoriteToggle={() => {
-                  onFavoriteToggle(speaker.id);
-                }}
-              />
-            );
-          })}
-        </div>
+
+  if (hasErrored === true) {
+    return (
+      <div className="text-danger">
+        ERROR:<b>loading Speaker Data Failed{error}</b>
       </div>
-    </div>
+    );
+  }
+  return (
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <div className="container speakers-list">
+            <div className="row">
+              {speakerData.map((speaker) => {
+                return (
+                  <Speaker
+                    key={speaker.id}
+                    speaker={speaker}
+                    showSessions={showSessions}
+                    onFavoriteToggle={() => {
+                      onFavoriteToggle(speaker.id);
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
