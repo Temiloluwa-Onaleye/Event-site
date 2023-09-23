f3016752f72362bb32f2f184f6315fd03c35992e
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-09-23 21:40:32 +0100

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "94a49ae85465fd60"; }
+/******/ 		__webpack_require__.h = function() { return "6fd2109d41a808b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
